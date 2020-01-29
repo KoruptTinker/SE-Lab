1fbccb95142e364f00430a51d3d64194f307e5d2
KoruptTinker <pisces1237@gmail.com>
2020-01-29 08:46:32 +0000

    First commit

---
+++ Lab1/ques1.java
@@ -0,0 +1,54 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class ques1 {
+    int size;
+    int[] arr;
+
+    public int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < this.size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public void bubble_recursive(int[] arr, int size) {
+        if (size == 1) {
+            return;
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        bubble_recursive(arr, size - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ques1 t = new ques1();
+        t.size = sc.nextInt();
+        t.arr = new int[t.size];
+        for (int i = 0; i < t.size; i++) {
+            t.arr[i] = sc.nextInt();
+        }
+        sc.close();
+        t.bubble_recursive(t.arr, t.size);
+        System.out.println();
+        for (int i = 0; i < t.size; i++) {
+            System.out.print(t.arr[i]);
+            System.out.print(",");
+        }
+        System.out.println();
+    }
+}
