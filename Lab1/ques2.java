0ad79cea563deed495e260cdc46f0d5f1ac35bb9
KoruptTinker <pisces1237@gmail.com>
2020-01-29 09:15:19 +0000

    Initial Commit

---
+++ Lab1/ques2.java
@@ -0,0 +1,39 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class ques2 {
+    int size;
+    int[] arr;
+
+    public void insertionSort(int[] arr) {
+        int key, j;
+        for (int i = 1; i < arr.length; i++) {
+            key = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ques2 t = new ques2();
+        t.size = sc.nextInt();
+        t.arr = new int[t.size];
+        for (int i = 0; i < t.size; i++) {
+            t.arr[i] = sc.nextInt();
+        }
+        sc.close();
+        t.insertionSort(t.arr);
+        System.out.println();
+        for (int i = 0; i < t.size; i++) {
+            System.out.print(t.arr[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
